df6ce409554ee75f84101b889740a39fae25048d
seanlilmateus <seanlilmateus@yahoo.de>
2014-07-07 12:34:17 +0200

    url label added on the buttom of the screen

--- app/controllers/window_controller.rb
+++ app/controllers/window_controller.rb
@@ -32,7 +32,12 @@ module Luke
         lbl.opaque = false
         lbl.textColor = NSColor.whiteColor
         lbl.alignment = NSCenterTextAlignment
-        lbl.backgroundColor = NSColor.clearColor
+        lbl.cell.backgroundColor = NSColor.blackColor
+        lbl.cell.usesSingleLineMode = true
+        lbl.cell.wraps = true
+        lbl.cell.lineBreakMode = NSLineBreakByTruncatingMiddle
+        lbl.preferredMaxLayoutWidth = 300
+        # lbl.backgroundColor = NSColor.clearColor
       end
     end
     
@@ -50,28 +55,28 @@ module Luke
     
     
     def icons_matrix(num_of_rows=1, num_of_cols=1)
-      @_matrix ||= IconsMatrix.new([[0, 0], [145, 145]], num_of_rows, num_of_cols).tap do |m|
+      @_matrix ||= IconsMatrix.new([[0, 0], [145, 145]], num_of_rows, num_of_cols).tap do |mtx|
         Motion::Layout.new do |layout|
           layout.view transparent_view
-          layout.subviews matrix: m, gear: settings_button, label:url_label
-          layout.vertical '|-15-[matrix]-30-|'
+          layout.subviews matrix: mtx, gear: settings_button, label:url_label
+          layout.vertical   '|-15-[matrix]-30-|'
           layout.horizontal '|-5-[matrix]-5-|'
-          layout.vertical '|-10-[gear(20)]'
+          layout.vertical   '|-10-[gear(20)]'
           layout.horizontal '|-10-[gear(20)]'
-          layout.horizontal '|[label]|'
-          layout.vertical '[label]-5-|'
+          layout.horizontal '[label(<=300@1000)]'
+          layout.vertical   '[label(==20@1000)]-2-|'
+          layout.constraint { label.centerX == superview.centerX }
         end
       end
     end
     
     def add_items(items)
       slices = items.each_slice(4).to_a
-      num_of_cols = slices.count #- 1
+      num_of_cols = slices.count
       num_of_rows = num_of_cols > 1 ? 4 : slices.first.count
       items.zip(icons_matrix(num_of_cols, num_of_rows).cells) do |app, cell|
         cell.title, cell.image = app[:name], app[:icon]
       end
-      
       icons_matrix.cells.each_with_index { |cell, idx| cell.tag = idx }      
       icons_matrix.sizeToCells
     end
